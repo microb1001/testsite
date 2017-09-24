6afb350ad9c662d7318fc1565c3623ceaba3a18a
i1 <void1@kubachi.ru>
2017-09-24 22:24:47 +0300

    add file as main2 (load - save)

---
+++ mainCSV.go
@@ -0,0 +1,8 @@
+package main
+
+// images specifies the site content: a collection of images.   https://lenta.ru/
+var images = map[string]*Image{
+	"go":     {"The Go Gopher", "https://golang.org/doc/gopher/frontpage.png"},
+	"google": {"The Google Logo", "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+	"lenta": {"The Lenta Logo", "https://icdn.lenta.ru/images/2017/09/20/18/20170920184345749/pic_9236d1c3d84b722a85fe66166a0ef251.jpg"},
+}
