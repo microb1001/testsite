81602e926a24a81a807623e3df083780c58bf37d
i1 <void1@kubachi.ru>
2017-09-26 16:20:35 +0300

    26.09.17

--- csv.go
+++ csv.go
@@ -52,17 +52,46 @@ func dump(datasets interface{}, fname string) {
 
 func parse(get_csv_to interface{}, fname string) {
 
+	type pkeytype struct {
+		pkey, pmap  int
+	}
+	var pkey []pkeytype
+	var tempmap map[string]int
+	tempmap = make(map[string]int)
+	items := reflect.ValueOf(get_csv_to).Elem()
+	argtype :=items.Type()
+	structtype:=argtype.Elem()
+
 	csvFile, err := os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
 		}
 	r := csv.NewReader(bufio.NewReader(csvFile))
 	r.Comma = ';'
+	lineheader, err := r.Read()
+	if err != nil {
+		log.Fatal(err)
+		}
+	for i, num:= range lineheader {
+		tempmap[num]=i
+	}
+	fmt.Println(lineheader)
+	fmt.Println(structtype)
 
-	items := reflect.ValueOf(get_csv_to).Elem()
-	itemstype :=items.Type()
-	fmt.Println(itemstype)
-	items.Set(reflect.MakeSlice(itemstype,1,1000))
+	for i := 0; i < structtype.NumField(); i++ {
+		if a,ok:=structtype.Field(i).Tag.Lookup("csv");ok {
+			b,ok:=tempmap[a]
+			if ok != true {
+				log.Fatal(err)
+			}
+			pkey=append(pkey,pkeytype{i,b})
+			fmt.Println(a)
+		}
+		//fmt.Println("===",v.Type().Field(j).Name, v.Field(j).Interface())
+	}
+	fmt.Println(pkey)
+	fmt.Println(argtype)
+	items.Set(reflect.MakeSlice(argtype,1,1000))
 	var people []good
 	for {
 		line, err := r.Read()
@@ -71,12 +100,15 @@ func parse(get_csv_to interface{}, fname string) {
 		} else if err != nil {
 			log.Fatal(err)
 		}
-		people = append(people, good{
-			Articul: line[0],
-			Info:  line[1],
-			Image: line[2],
-		})
-		//fmt.Println(line)
+
+		good1:= reflect.New(structtype)
+		good2:=good1.Elem()
+		for _,k:=range pkey {
+		 good2.Field(k.pkey).SetString(line[k.pmap])
+		}
+		//people = append(people, good1)
+		fmt.Println(line)
+		items.Set(reflect.Append(items,good2))
 	}
 
 	fmt.Println(people)
@@ -91,7 +123,7 @@ func parse(get_csv_to interface{}, fname string) {
 			if item.Kind() == reflect.Struct {
 				v := reflect.Indirect(item)
 				for j := 0; j < v.NumField(); j++ {
-					v.Field(j).SetString("1039")
+					//v.Field(j).SetString("1039")
 					fmt.Println("===",v.Type().Field(j).Name, v.Field(j).Interface())
 				}
 			}
