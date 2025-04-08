599616606a74444c21af50843a39e3e9f6116bcf
Microb1001 <void@kubachi.ru>
2025-04-08 09:25:04 +0300

    tst

--- main.go
+++ main.go
@@ -4,20 +4,22 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
-	"strconv"
+
+	"./mydb/cart7"
+	"./mydb/good7"
+	"./templates/tdata"
+	_ "./webelements/escape"
+	"./webelements/myfs"
+	"./webelements/pager"
 	"./webelements/session"
 	"./webelements/sphinx"
-	"./webelements/pager"
-	_"./webelements/escape"
-	"./webelements/myfs"
-	"./mydb/good7"
-	"./mydb/cart7"
-	"./templates/tdata"
 )
 
-const items_per_page=10
+const items_per_page = 10
+
 var goods good7.Goods
 var userCart map[uint64]cart7.Cart
 var sphi sphinx.Sphinx
@@ -27,7 +29,7 @@ func main() {
 	goods.AddPrice("")
 	sphi.Init()
 	sphi.Add(&goods)
-	//mycsv.Dump(goods)
+	//mycsv.Dump(goods) -tst
 
 	userCart = make(map[uint64]cart7.Cart)
 
@@ -35,11 +37,11 @@ func main() {
 	http.HandleFunc("/product/", imageHandler)
 	http.HandleFunc("/cart/", cartHandler)
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(
-												myfs.FileSystem{http.Dir("img/")})))
+		myfs.FileSystem{http.Dir("img/")})))
 	log.Fatal(http.ListenAndServe("localhost:80", nil))
 }
 
-var mainTemplate = template.Must(template.ParseFiles("templates/index.tmpl","templates/paginator.tmpl"))
+var mainTemplate = template.Must(template.ParseFiles("templates/index.tmpl", "templates/paginator.tmpl"))
 var imageTemplate = template.Must(template.Must(mainTemplate.Clone()).ParseFiles("templates/image.tmpl"))
 var cartTemplate = template.Must(template.Must(mainTemplate.Clone()).ParseFiles("templates/cart.tmpl"))
 
@@ -49,13 +51,13 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	var data tdata.List
 	var p []string
 	start := time.Now()
-	data.Session = session.Get(w,r)
+	data.Session = session.Get(w, r)
 	goods.Mu.RLock()
 	defer goods.Mu.RUnlock()
 
 	searchstring := r.FormValue("text")
 	if searchstring != "" {
-		p=append(p,"text="+searchstring)
+		p = append(p, "text="+searchstring)
 	}
 
 	//_=r.ParseForm() // Само вызывается из FormValue
@@ -65,40 +67,39 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	var pageCurrent int
 	var mainPage string
 
-	data.Links =make([]tdata.ListItem,0,items_per_page)
-	data.Title= "Image gallery 11-11"
+	data.Links = make([]tdata.ListItem, 0, items_per_page)
+	data.Title = "Image gallery 11-11"
 	data.Body = ""
-	mainPage=r.URL.Path // !!Нужна обработка пользовательского ввода!!
-	if mainPage=="/search/" {
-		goods.Sel[mainPage]= sphi.Find(searchstring)
+	mainPage = r.URL.Path // !!Нужна обработка пользовательского ввода!!
+	if mainPage == "/search/" {
+		goods.Sel[mainPage] = sphi.Find(searchstring)
 	}
-	pageCurrent,err:=strconv.Atoi(r.FormValue("p"))
+	pageCurrent, err := strconv.Atoi(r.FormValue("p"))
 	if err != nil {
 		pageCurrent = 0
 	}
 	var l, h int
-	mainPage2:=mainPage+"?"
-	for _,i:=range p{
-		mainPage2=mainPage2+"&"+i
+	mainPage2 := mainPage + "?"
+	for _, i := range p {
+		mainPage2 = mainPage2 + "&" + i
 	}
-	data.Pager, l, h = pager.Set(pageCurrent,items_per_page, len(goods.Sel[mainPage]), mainPage2+"&")
-	for _,i := range goods.Sel[mainPage][l:h] {
-		data.Links = append(data.Links, tdata.ListItem{ "/product/" + goods.O[i].VendorCode, "/cart/?additem=" + goods.O[i].VendorCode,"","/images/400/"+ goods.O[i].VendorCode+".jpg",goods.O[i].Brief,goods.O[i].Description})
+	data.Pager, l, h = pager.Set(pageCurrent, items_per_page, len(goods.Sel[mainPage]), mainPage2+"&")
+	for _, i := range goods.Sel[mainPage][l:h] {
+		data.Links = append(data.Links, tdata.ListItem{"/product/" + goods.O[i].VendorCode, "/cart/?additem=" + goods.O[i].VendorCode, "", "/images/400/" + goods.O[i].VendorCode + ".jpg", goods.O[i].Brief, goods.O[i].Description})
 	}
 
-	data.Cat=goods.Category1list
-	data.Timer=time.Now().Sub(start)
+	data.Cat = goods.Category1list
+	data.Timer = time.Now().Sub(start)
 	if err := mainTemplate.Execute(w, data); err != nil {
 		log.Println(err)
 	}
 	t := time.Now()
 	elapsed := t.Sub(start)
-	log.Println("timer ",cnt, elapsed)
+	log.Println("timer ", cnt, elapsed)
 }
 
 // imageHandler is an HTTP handler that serves the info about good.
 func imageHandler(w http.ResponseWriter, r *http.Request) {
-
 
 	dataindex, ok := goods.Goodsmap[strings.TrimPrefix(r.URL.Path, "/product/")]
 	//data, ok := images[strings.TrimPrefix(r.URL.Path, "/product/")]
@@ -108,7 +109,7 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var data tdata.Product = tdata.Product{
-		[]tdata.ProductSpec{}, "", "/images/" + goods.O[dataindex].VendorCode + ".jpg", "",goods.O[dataindex].Description,session.Get(w, r)}
+		[]tdata.ProductSpec{}, "", "/images/" + goods.O[dataindex].VendorCode + ".jpg", "", goods.O[dataindex].Description, session.Get(w, r)}
 	for _, item3 := range []string{"Высота", "Ширина", "Диаметр", "Размер"} {
 		if goods.O[dataindex].Spec[item3] != "" {
 			data.Spec1 = append(data.Spec1, tdata.ProductSpec{item3, goods.O[dataindex].Spec[item3]})
@@ -127,16 +128,16 @@ func cartHandler(w http.ResponseWriter, r *http.Request) {
 	data.Session = sessid
 	if r.FormValue("additem") != "" {
 		goodsid := goods.Goodsmap[r.FormValue("additem")]
-		userCart[sessid] = append(userCart[sessid], cart7.Elem{"+",1,goods.O[goodsid]}) // refresh добавляет повтор убрать (можно через реферрер
+		userCart[sessid] = append(userCart[sessid], cart7.Elem{"+", 1, goods.O[goodsid]}) // refresh добавляет повтор убрать (можно через реферрер
 	}
-	for _,kk := range userCart[sessid]{
-		data.UserCart = append(data.UserCart,tdata.CartElem{"","","","/images/200/"+ kk.VendorCode+".jpg",kk.Brief,kk.Description,kk.VendorCode,kk.Price})
+	for _, kk := range userCart[sessid] {
+		data.UserCart = append(data.UserCart, tdata.CartElem{"", "", "", "/images/200/" + kk.VendorCode + ".jpg", kk.Brief, kk.Description, kk.VendorCode, kk.Price})
 	}
 	data.TotalPrice = 0
-	data.TotalCount =0
-	for _,m:= range userCart[sessid]{
-	 data.TotalPrice+=m.Price
-	 data.TotalCount+=1
+	data.TotalCount = 0
+	for _, m := range userCart[sessid] {
+		data.TotalPrice += m.Price
+		data.TotalCount += 1
 	}
 	if err := cartTemplate.Execute(w, data); err != nil {
 		log.Println(err)
